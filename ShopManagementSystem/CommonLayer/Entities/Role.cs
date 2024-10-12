968ec4fc7a85edbb39a452e028c4b78e9d2c680b
Kacruz2004 <kc.563w@gmail.com>
2024-10-12 08:28:40 -0600

    CArpeta entities y Sql Data Access

---
+++ ShopManagementSystem/CommonLayer/Entities/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLayer.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public int Type { get; set; }
+        public string Details {  get; set; }
+
+    }
+}
--- ShopManagementSystem/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ ShopManagementSystem/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e72ca870f5774786d85b7481ba190ae98ae39c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
