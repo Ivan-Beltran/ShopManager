70af514164a9ff1620e113441d2a47cc3101c401
ivan <ivanbeltran1610@gmail.com>
2024-10-12 18:52:57 -0600

    subiendo los cambis que realizo kevin

---
+++ ShopManagementSystem/CommonLayer/Entities/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLayer.Entities
+{
+   public class Products
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public string Quantity { get; set; }
+        public string PurchaseDate { get; set; }
+    }
+}
