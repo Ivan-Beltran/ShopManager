3c86490d55a00d43981c9eed3673e304925655b1
Stefany <u20230748@univo.edu.sv>
2024-11-02 16:52:56 -0600

    Agregando TextBoxs para registro de empleados

--- ShopManagementSystem/PresentationLayer/Forms/ViewEmployeesForm.Designer.cs
+++ ShopManagementSystem/PresentationLayer/Forms/ViewEmployeesForm.Designer.cs
@@ -37,9 +37,20 @@
             btnAddEmployee = new Button();
             btnEditEmployee = new Button();
             btnDeteleEmployee = new Button();
-            addEmployeeTextBox = new TextBox();
+            txtName = new TextBox();
             boxEditEmployee = new ComboBox();
             boxDeteleEmployee = new ComboBox();
+            nameEmployee = new Label();
+            lastNameEmployee = new Label();
+            txtLastName = new TextBox();
+            passwordEmployee = new Label();
+            txtPassword = new TextBox();
+            usuarioEmployee = new Label();
+            txtUsuario = new TextBox();
+            emailEmployee = new Label();
+            txtGmail = new TextBox();
+            duiEmployee = new Label();
+            txtDui = new TextBox();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)emplyeesIconPictureBox).BeginInit();
             ((System.ComponentModel.ISupportInitialize)EmployeesDataGridView).BeginInit();
@@ -51,20 +62,18 @@
             panel1.Controls.Add(emplyeesIconPictureBox);
             panel1.Controls.Add(searchEmployeeTextBox);
             panel1.Controls.Add(searchEmployeeLabel);
-            panel1.Location = new Point(-35, -3);
-            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Location = new Point(-31, -2);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1431, 85);
+            panel1.Size = new Size(1756, 64);
             panel1.TabIndex = 1;
             // 
             // emplyeesIconPictureBox
             // 
             emplyeesIconPictureBox.BackColor = Color.FromArgb(224, 224, 224);
             emplyeesIconPictureBox.Image = (Image)resources.GetObject("emplyeesIconPictureBox.Image");
-            emplyeesIconPictureBox.Location = new Point(112, 15);
-            emplyeesIconPictureBox.Margin = new Padding(3, 4, 3, 4);
+            emplyeesIconPictureBox.Location = new Point(98, 11);
             emplyeesIconPictureBox.Name = "emplyeesIconPictureBox";
-            emplyeesIconPictureBox.Size = new Size(114, 67);
+            emplyeesIconPictureBox.Size = new Size(100, 50);
             emplyeesIconPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
             emplyeesIconPictureBox.TabIndex = 9;
             emplyeesIconPictureBox.TabStop = false;
@@ -72,10 +81,9 @@
             // searchEmployeeTextBox
             // 
             searchEmployeeTextBox.FormattingEnabled = true;
-            searchEmployeeTextBox.Location = new Point(456, 28);
-            searchEmployeeTextBox.Margin = new Padding(3, 4, 3, 4);
+            searchEmployeeTextBox.Location = new Point(399, 21);
             searchEmployeeTextBox.Name = "searchEmployeeTextBox";
-            searchEmployeeTextBox.Size = new Size(548, 28);
+            searchEmployeeTextBox.Size = new Size(480, 23);
             searchEmployeeTextBox.TabIndex = 2;
             // 
             // searchEmployeeLabel
@@ -84,9 +92,9 @@
             searchEmployeeLabel.BackColor = Color.WhiteSmoke;
             searchEmployeeLabel.Font = new Font("Segoe UI Historic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             searchEmployeeLabel.ForeColor = SystemColors.ActiveCaptionText;
-            searchEmployeeLabel.Location = new Point(233, 28);
+            searchEmployeeLabel.Location = new Point(204, 21);
             searchEmployeeLabel.Name = "searchEmployeeLabel";
-            searchEmployeeLabel.Size = new Size(211, 32);
+            searchEmployeeLabel.Size = new Size(172, 25);
             searchEmployeeLabel.TabIndex = 0;
             searchEmployeeLabel.Text = "Buscar Empleado";
             // 
@@ -97,11 +105,10 @@
             EmployeesDataGridView.BackgroundColor = Color.Lavender;
             EmployeesDataGridView.ColumnHeadersHeight = 29;
             EmployeesDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            EmployeesDataGridView.Location = new Point(27, 141);
-            EmployeesDataGridView.Margin = new Padding(3, 4, 3, 4);
+            EmployeesDataGridView.Location = new Point(24, 106);
             EmployeesDataGridView.Name = "EmployeesDataGridView";
             EmployeesDataGridView.RowHeadersWidth = 51;
-            EmployeesDataGridView.Size = new Size(1369, 336);
+            EmployeesDataGridView.Size = new Size(1198, 252);
             EmployeesDataGridView.TabIndex = 2;
             // 
             // btnAddEmployee
@@ -109,10 +116,9 @@
             btnAddEmployee.BackColor = Color.FromArgb(0, 166, 225);
             btnAddEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
             btnAddEmployee.ForeColor = Color.White;
-            btnAddEmployee.Location = new Point(819, 563);
-            btnAddEmployee.Margin = new Padding(3, 4, 3, 4);
+            btnAddEmployee.Location = new Point(1178, 509);
             btnAddEmployee.Name = "btnAddEmployee";
-            btnAddEmployee.Size = new Size(263, 49);
+            btnAddEmployee.Size = new Size(262, 56);
             btnAddEmployee.TabIndex = 3;
             btnAddEmployee.Text = "Agregar nuevo empledo";
             btnAddEmployee.UseVisualStyleBackColor = false;
@@ -122,10 +128,9 @@
             btnEditEmployee.BackColor = Color.FromArgb(0, 166, 225);
             btnEditEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
             btnEditEmployee.ForeColor = Color.White;
-            btnEditEmployee.Location = new Point(819, 663);
-            btnEditEmployee.Margin = new Padding(3, 4, 3, 4);
+            btnEditEmployee.Location = new Point(1562, 134);
             btnEditEmployee.Name = "btnEditEmployee";
-            btnEditEmployee.Size = new Size(263, 49);
+            btnEditEmployee.Size = new Size(130, 60);
             btnEditEmployee.TabIndex = 4;
             btnEditEmployee.Text = "Editar empleado";
             btnEditEmployee.UseVisualStyleBackColor = false;
@@ -135,56 +140,164 @@
             btnDeteleEmployee.BackColor = Color.FromArgb(0, 166, 225);
             btnDeteleEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
             btnDeteleEmployee.ForeColor = Color.White;
-            btnDeteleEmployee.Location = new Point(819, 767);
-            btnDeteleEmployee.Margin = new Padding(3, 4, 3, 4);
+            btnDeteleEmployee.Location = new Point(1562, 246);
             btnDeteleEmployee.Name = "btnDeteleEmployee";
-            btnDeteleEmployee.Size = new Size(263, 49);
+            btnDeteleEmployee.Size = new Size(130, 60);
             btnDeteleEmployee.TabIndex = 5;
             btnDeteleEmployee.Text = "Borrar empleado";
             btnDeteleEmployee.UseVisualStyleBackColor = false;
             // 
-            // addEmployeeTextBox
+            // txtName
             // 
-            addEmployeeTextBox.Location = new Point(98, 575);
-            addEmployeeTextBox.Margin = new Padding(3, 4, 3, 4);
-            addEmployeeTextBox.Name = "addEmployeeTextBox";
-            addEmployeeTextBox.Size = new Size(548, 27);
-            addEmployeeTextBox.TabIndex = 6;
+            txtName.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            txtName.Location = new Point(122, 496);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(201, 27);
+            txtName.TabIndex = 6;
             // 
             // boxEditEmployee
             // 
             boxEditEmployee.FormattingEnabled = true;
-            boxEditEmployee.Location = new Point(98, 675);
-            boxEditEmployee.Margin = new Padding(3, 4, 3, 4);
+            boxEditEmployee.Location = new Point(1239, 155);
             boxEditEmployee.Name = "boxEditEmployee";
-            boxEditEmployee.Size = new Size(548, 28);
+            boxEditEmployee.Size = new Size(291, 23);
             boxEditEmployee.TabIndex = 7;
             // 
             // boxDeteleEmployee
             // 
             boxDeteleEmployee.FormattingEnabled = true;
-            boxDeteleEmployee.Location = new Point(98, 779);
-            boxDeteleEmployee.Margin = new Padding(3, 4, 3, 4);
+            boxDeteleEmployee.Location = new Point(1239, 267);
             boxDeteleEmployee.Name = "boxDeteleEmployee";
-            boxDeteleEmployee.Size = new Size(548, 28);
+            boxDeteleEmployee.Size = new Size(291, 23);
             boxDeteleEmployee.TabIndex = 8;
+            // 
+            // nameEmployee
+            // 
+            nameEmployee.AutoSize = true;
+            nameEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            nameEmployee.Location = new Point(25, 496);
+            nameEmployee.Name = "nameEmployee";
+            nameEmployee.Size = new Size(74, 20);
+            nameEmployee.TabIndex = 9;
+            nameEmployee.Text = "Nombre:";
+            // 
+            // lastNameEmployee
+            // 
+            lastNameEmployee.AutoSize = true;
+            lastNameEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            lastNameEmployee.Location = new Point(25, 550);
+            lastNameEmployee.Name = "lastNameEmployee";
+            lastNameEmployee.Size = new Size(78, 20);
+            lastNameEmployee.TabIndex = 11;
+            lastNameEmployee.Text = "Apellido:";
+            // 
+            // txtLastName
+            // 
+            txtLastName.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            txtLastName.Location = new Point(122, 550);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(201, 27);
+            txtLastName.TabIndex = 10;
+            // 
+            // passwordEmployee
+            // 
+            passwordEmployee.AutoSize = true;
+            passwordEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            passwordEmployee.Location = new Point(382, 553);
+            passwordEmployee.Name = "passwordEmployee";
+            passwordEmployee.Size = new Size(97, 20);
+            passwordEmployee.TabIndex = 15;
+            passwordEmployee.Text = "Contraseña:";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            txtPassword.Location = new Point(479, 553);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(201, 27);
+            txtPassword.TabIndex = 14;
+            // 
+            // usuarioEmployee
+            // 
+            usuarioEmployee.AutoSize = true;
+            usuarioEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            usuarioEmployee.Location = new Point(382, 499);
+            usuarioEmployee.Name = "usuarioEmployee";
+            usuarioEmployee.Size = new Size(70, 20);
+            usuarioEmployee.TabIndex = 13;
+            usuarioEmployee.Text = "Usuario:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            txtUsuario.Location = new Point(479, 499);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(201, 27);
+            txtUsuario.TabIndex = 12;
+            // 
+            // emailEmployee
+            // 
+            emailEmployee.AutoSize = true;
+            emailEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            emailEmployee.Location = new Point(728, 556);
+            emailEmployee.Name = "emailEmployee";
+            emailEmployee.Size = new Size(57, 20);
+            emailEmployee.TabIndex = 19;
+            emailEmployee.Text = "Gmail:";
+            // 
+            // txtGmail
+            // 
+            txtGmail.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            txtGmail.Location = new Point(825, 556);
+            txtGmail.Name = "txtGmail";
+            txtGmail.Size = new Size(201, 27);
+            txtGmail.TabIndex = 18;
+            // 
+            // duiEmployee
+            // 
+            duiEmployee.AutoSize = true;
+            duiEmployee.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            duiEmployee.Location = new Point(728, 502);
+            duiEmployee.Name = "duiEmployee";
+            duiEmployee.Size = new Size(41, 20);
+            duiEmployee.TabIndex = 17;
+            duiEmployee.Text = "DUI:";
+            // 
+            // txtDui
+            // 
+            txtDui.Font = new Font("Segoe UI Historic", 11F, FontStyle.Bold);
+            txtDui.Location = new Point(825, 502);
+            txtDui.Name = "txtDui";
+            txtDui.Size = new Size(201, 27);
+            txtDui.TabIndex = 16;
             // 
             // ViewEmployeesForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1419, 872);
+            ClientSize = new Size(1723, 706);
+            Controls.Add(emailEmployee);
+            Controls.Add(txtGmail);
+            Controls.Add(duiEmployee);
+            Controls.Add(txtDui);
+            Controls.Add(passwordEmployee);
+            Controls.Add(txtPassword);
+            Controls.Add(usuarioEmployee);
+            Controls.Add(txtUsuario);
+            Controls.Add(lastNameEmployee);
+            Controls.Add(txtLastName);
+            Controls.Add(nameEmployee);
             Controls.Add(boxDeteleEmployee);
             Controls.Add(boxEditEmployee);
-            Controls.Add(addEmployeeTextBox);
+            Controls.Add(txtName);
             Controls.Add(btnDeteleEmployee);
             Controls.Add(btnEditEmployee);
             Controls.Add(btnAddEmployee);
             Controls.Add(EmployeesDataGridView);
             Controls.Add(panel1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "ViewEmployeesForm";
             Text = "ViewEmployees";
+            WindowState = FormWindowState.Maximized;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)emplyeesIconPictureBox).EndInit();
@@ -201,9 +314,20 @@
         private Button btnAddEmployee;
         private Button btnEditEmployee;
         private Button btnDeteleEmployee;
-        private TextBox addEmployeeTextBox;
+        private TextBox txtName;
         private ComboBox boxEditEmployee;
         private ComboBox boxDeteleEmployee;
         private PictureBox emplyeesIconPictureBox;
+        private Label nameEmployee;
+        private Label lastNameEmployee;
+        private TextBox txtLastName;
+        private Label passwordEmployee;
+        private TextBox txtPassword;
+        private Label usuarioEmployee;
+        private TextBox txtUsuario;
+        private Label emailEmployee;
+        private TextBox txtGmail;
+        private Label duiEmployee;
+        private TextBox txtDui;
     }
 }
