fd0fb52b3c6eeb3a5b45f1ce9a6512533287607e
Stefany <u20230748@univo.edu.sv>
2024-11-15 23:22:26 -0600

    Agregando tabla en el form para proveedores

--- ShopManagementSystem/PresentationLayer/Forms/RegisterSuppliersForm.Designer.cs
+++ ShopManagementSystem/PresentationLayer/Forms/RegisterSuppliersForm.Designer.cs
@@ -28,28 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterSuppliersForm));
             suppliersLabel = new Label();
             nameSupplierLabel = new Label();
-            txtNameSupplier = new TextBox();
-            txtPhoneSupplier = new TextBox();
+            nameSupplierTextBox = new TextBox();
+            phoneSupplierTextBox = new TextBox();
             phoneSupplierLabel = new Label();
-            txtEmailSupplier = new TextBox();
+            emailSupplierTextBox = new TextBox();
             emailSupplierLabel = new Label();
-            txtAddressSupplier = new TextBox();
+            addressSupplierTextBox = new TextBox();
             addressSupplierLabel = new Label();
             registerSupplierButton = new FontAwesome.Sharp.IconButton();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SupplierDataGridView = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)SupplierDataGridView).BeginInit();
             SuspendLayout();
             // 
             // suppliersLabel
             // 
             suppliersLabel.AutoSize = true;
             suppliersLabel.Font = new Font("Segoe UI Historic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            suppliersLabel.Location = new Point(594, 64);
+            suppliersLabel.Location = new Point(145, 67);
             suppliersLabel.Name = "suppliersLabel";
-            suppliersLabel.Size = new Size(242, 32);
+            suppliersLabel.Size = new Size(198, 25);
             suppliersLabel.TabIndex = 1;
             suppliersLabel.Text = "Registrar Proveedor";
             // 
@@ -57,71 +56,67 @@
             // 
             nameSupplierLabel.AutoSize = true;
             nameSupplierLabel.Font = new Font("Segoe UI Historic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            nameSupplierLabel.Location = new Point(550, 195);
+            nameSupplierLabel.Location = new Point(106, 165);
             nameSupplierLabel.Name = "nameSupplierLabel";
-            nameSupplierLabel.Size = new Size(85, 25);
+            nameSupplierLabel.Size = new Size(67, 20);
             nameSupplierLabel.TabIndex = 2;
             nameSupplierLabel.Text = "Nombre:";
             // 
-            // txtNameSupplier
+            // nameSupplierTextBox
             // 
-            txtNameSupplier.Location = new Point(705, 184);
-            txtNameSupplier.Margin = new Padding(3, 4, 3, 4);
-            txtNameSupplier.Name = "txtNameSupplier";
-            txtNameSupplier.Size = new Size(243, 27);
-            txtNameSupplier.TabIndex = 3;
+            nameSupplierTextBox.Location = new Point(242, 157);
+            nameSupplierTextBox.Name = "nameSupplierTextBox";
+            nameSupplierTextBox.Size = new Size(213, 23);
+            nameSupplierTextBox.TabIndex = 3;
             // 
-            // txtPhoneSupplier
+            // phoneSupplierTextBox
             // 
-            txtPhoneSupplier.Location = new Point(705, 275);
-            txtPhoneSupplier.Margin = new Padding(3, 4, 3, 4);
-            txtPhoneSupplier.Name = "txtPhoneSupplier";
-            txtPhoneSupplier.Size = new Size(243, 27);
-            txtPhoneSupplier.TabIndex = 5;
+            phoneSupplierTextBox.Location = new Point(242, 225);
+            phoneSupplierTextBox.Name = "phoneSupplierTextBox";
+            phoneSupplierTextBox.Size = new Size(213, 23);
+            phoneSupplierTextBox.TabIndex = 5;
             // 
             // phoneSupplierLabel
             // 
             phoneSupplierLabel.AutoSize = true;
             phoneSupplierLabel.Font = new Font("Segoe UI Historic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            phoneSupplierLabel.Location = new Point(550, 279);
+            phoneSupplierLabel.Location = new Point(106, 228);
             phoneSupplierLabel.Name = "phoneSupplierLabel";
-            phoneSupplierLabel.Size = new Size(90, 25);
+            phoneSupplierLabel.Size = new Size(71, 20);
             phoneSupplierLabel.TabIndex = 4;
             phoneSupplierLabel.Text = "Teléfono:";
             // 
-            // txtEmailSupplier
+            // emailSupplierTextBox
             // 
-            txtEmailSupplier.Location = new Point(705, 368);
-            txtEmailSupplier.Margin = new Padding(3, 4, 3, 4);
-            txtEmailSupplier.Name = "txtEmailSupplier";
-            txtEmailSupplier.Size = new Size(243, 27);
-            txtEmailSupplier.TabIndex = 7;
+            emailSupplierTextBox.Location = new Point(242, 295);
+            emailSupplierTextBox.Name = "emailSupplierTextBox";
+            emailSupplierTextBox.Size = new Size(213, 23);
+            emailSupplierTextBox.TabIndex = 7;
             // 
             // emailSupplierLabel
             // 
             emailSupplierLabel.AutoSize = true;
             emailSupplierLabel.Font = new Font("Segoe UI Historic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            emailSupplierLabel.Location = new Point(550, 372);
+            emailSupplierLabel.Location = new Point(106, 298);
             emailSupplierLabel.Name = "emailSupplierLabel";
-            emailSupplierLabel.Size = new Size(65, 25);
+            emailSupplierLabel.Size = new Size(51, 20);
             emailSupplierLabel.TabIndex = 6;
             emailSupplierLabel.Text = "Gmail:";
             // 
-            // txtAddressSupplier
+            // addressSupplierTextBox
             // 
-            txtAddressSupplier.Location = new Point(705, 465);
-            txtAddressSupplier.Margin = new Padding(3, 4, 3, 4);
-            txtAddressSupplier.Name = "txtAddressSupplier";
-            txtAddressSupplier.Size = new Size(243, 27);
-            txtAddressSupplier.TabIndex = 9;
+            addressSupplierTextBox.Location = new Point(242, 368);
+            addressSupplierTextBox.Name = "addressSupplierTextBox";
+            addressSupplierTextBox.Size = new Size(213, 23);
+            addressSupplierTextBox.TabIndex = 9;
             // 
             // addressSupplierLabel
             // 
             addressSupplierLabel.AutoSize = true;
             addressSupplierLabel.Font = new Font("Segoe UI Historic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            addressSupplierLabel.Location = new Point(550, 469);
+            addressSupplierLabel.Location = new Point(106, 371);
             addressSupplierLabel.Name = "addressSupplierLabel";
-            addressSupplierLabel.Size = new Size(96, 25);
+            addressSupplierLabel.Size = new Size(75, 20);
             addressSupplierLabel.TabIndex = 8;
             addressSupplierLabel.Text = "Dirección:";
             // 
@@ -133,47 +128,42 @@
             registerSupplierButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
             registerSupplierButton.IconSize = 32;
             registerSupplierButton.ImageAlign = ContentAlignment.MiddleLeft;
-            registerSupplierButton.Location = new Point(647, 575);
-            registerSupplierButton.Margin = new Padding(3, 4, 3, 4);
+            registerSupplierButton.Location = new Point(191, 450);
             registerSupplierButton.Name = "registerSupplierButton";
-            registerSupplierButton.Size = new Size(174, 49);
+            registerSupplierButton.Size = new Size(152, 37);
             registerSupplierButton.TabIndex = 10;
             registerSupplierButton.Text = "Guardar";
             registerSupplierButton.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // SupplierDataGridView
             // 
-            pictureBox1.Anchor = AnchorStyles.None;
-            pictureBox1.BackColor = Color.Transparent;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(137, 167);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(376, 367);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 11;
-            pictureBox1.TabStop = false;
+            SupplierDataGridView.BackgroundColor = Color.Azure;
+            SupplierDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            SupplierDataGridView.Location = new Point(503, 157);
+            SupplierDataGridView.Name = "SupplierDataGridView";
+            SupplierDataGridView.Size = new Size(811, 280);
+            SupplierDataGridView.TabIndex = 11;
             // 
             // RegisterSuppliersForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.AliceBlue;
-            ClientSize = new Size(1233, 813);
-            Controls.Add(pictureBox1);
+            ClientSize = new Size(1369, 610);
+            Controls.Add(SupplierDataGridView);
             Controls.Add(registerSupplierButton);
-            Controls.Add(txtAddressSupplier);
+            Controls.Add(addressSupplierTextBox);
             Controls.Add(addressSupplierLabel);
-            Controls.Add(txtEmailSupplier);
+            Controls.Add(emailSupplierTextBox);
             Controls.Add(emailSupplierLabel);
-            Controls.Add(txtPhoneSupplier);
+            Controls.Add(phoneSupplierTextBox);
             Controls.Add(phoneSupplierLabel);
-            Controls.Add(txtNameSupplier);
+            Controls.Add(nameSupplierTextBox);
             Controls.Add(nameSupplierLabel);
             Controls.Add(suppliersLabel);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "RegisterSuppliersForm";
             Text = "RegisterSuppliersForm";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)SupplierDataGridView).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -181,14 +171,14 @@
         #endregion
         private Label suppliersLabel;
         private Label nameSupplierLabel;
-        private TextBox txtNameSupplier;
-        private TextBox txtPhoneSupplier;
+        private TextBox nameSupplierTextBox;
+        private TextBox phoneSupplierTextBox;
         private Label phoneSupplierLabel;
-        private TextBox txtEmailSupplier;
+        private TextBox emailSupplierTextBox;
         private Label emailSupplierLabel;
-        private TextBox txtAddressSupplier;
+        private TextBox addressSupplierTextBox;
         private Label addressSupplierLabel;
         private FontAwesome.Sharp.IconButton registerSupplierButton;
-        private PictureBox pictureBox1;
+        private DataGridView SupplierDataGridView;
     }
 }
