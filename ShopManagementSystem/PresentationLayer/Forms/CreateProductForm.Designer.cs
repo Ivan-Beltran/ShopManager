decb9e59575e15f7bc06fb895adaaf306a966f74
Kc-2004 <kc200404@gmail.com>
2024-11-26 11:20:31 -0600

    Validacion de creatproductform

--- ShopManagementSystem/PresentationLayer/Forms/CreateProductForm.Designer.cs
+++ ShopManagementSystem/PresentationLayer/Forms/CreateProductForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             productTypeLabel = new Label();
             productBrandLabel = new Label();
             producModelLabel = new Label();
@@ -49,18 +50,20 @@
             openFileImgDialog = new OpenFileDialog();
             addProducButton = new FontAwesome.Sharp.IconButton();
             textBox1 = new TextBox();
+            validationErrorProvider = new ErrorProvider(components);
             ((System.ComponentModel.ISupportInitialize)productImgPictureBox).BeginInit();
             ((System.ComponentModel.ISupportInitialize)productsDataGridView).BeginInit();
             optionsGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)validationErrorProvider).BeginInit();
             SuspendLayout();
             // 
             // productTypeLabel
             // 
             productTypeLabel.AutoSize = true;
             productTypeLabel.Font = new Font("Segoe UI Historic", 9.75F);
-            productTypeLabel.Location = new Point(205, 47);
+            productTypeLabel.Location = new Point(179, 35);
             productTypeLabel.Name = "productTypeLabel";
-            productTypeLabel.Size = new Size(47, 23);
+            productTypeLabel.Size = new Size(37, 17);
             productTypeLabel.TabIndex = 0;
             productTypeLabel.Text = "Tipo:";
             // 
@@ -68,9 +71,9 @@
             // 
             productBrandLabel.AutoSize = true;
             productBrandLabel.Font = new Font("Segoe UI Historic", 9.75F);
-            productBrandLabel.Location = new Point(205, 116);
+            productBrandLabel.Location = new Point(179, 87);
             productBrandLabel.Name = "productBrandLabel";
-            productBrandLabel.Size = new Size(61, 23);
+            productBrandLabel.Size = new Size(48, 17);
             productBrandLabel.TabIndex = 1;
             productBrandLabel.Text = "Marca:";
             // 
@@ -78,9 +81,9 @@
             // 
             producModelLabel.AutoSize = true;
             producModelLabel.Font = new Font("Segoe UI Historic", 9.75F);
-            producModelLabel.Location = new Point(205, 177);
+            producModelLabel.Location = new Point(179, 133);
             producModelLabel.Name = "producModelLabel";
-            producModelLabel.Size = new Size(72, 23);
+            producModelLabel.Size = new Size(57, 17);
             producModelLabel.TabIndex = 2;
             producModelLabel.Text = "Modelo:";
             // 
@@ -88,9 +91,9 @@
             // 
             productVersionLabel.AutoSize = true;
             productVersionLabel.Font = new Font("Segoe UI Historic", 9.75F);
-            productVersionLabel.Location = new Point(205, 243);
+            productVersionLabel.Location = new Point(179, 182);
             productVersionLabel.Name = "productVersionLabel";
-            productVersionLabel.Size = new Size(71, 23);
+            productVersionLabel.Size = new Size(55, 17);
             productVersionLabel.TabIndex = 3;
             productVersionLabel.Text = "Versión:";
             // 
@@ -98,9 +101,9 @@
             // 
             productColorLabel.AutoSize = true;
             productColorLabel.Font = new Font("Segoe UI Historic", 9.75F);
-            productColorLabel.Location = new Point(205, 319);
+            productColorLabel.Location = new Point(179, 239);
             productColorLabel.Name = "productColorLabel";
-            productColorLabel.Size = new Size(55, 23);
+            productColorLabel.Size = new Size(43, 17);
             productColorLabel.TabIndex = 4;
             productColorLabel.Text = "Color:";
             // 
@@ -108,62 +111,69 @@
             // 
             producPriceLabel.AutoSize = true;
             producPriceLabel.Font = new Font("Segoe UI Historic", 9.75F);
-            producPriceLabel.Location = new Point(205, 391);
+            producPriceLabel.Location = new Point(179, 293);
             producPriceLabel.Name = "producPriceLabel";
-            producPriceLabel.Size = new Size(61, 23);
+            producPriceLabel.Size = new Size(47, 17);
             producPriceLabel.TabIndex = 5;
             producPriceLabel.Text = "Precio:";
             // 
             // productBrandTextBox
             // 
-            productBrandTextBox.Location = new Point(297, 112);
+            productBrandTextBox.Location = new Point(260, 84);
+            productBrandTextBox.Margin = new Padding(3, 2, 3, 2);
             productBrandTextBox.Name = "productBrandTextBox";
-            productBrandTextBox.Size = new Size(351, 27);
+            productBrandTextBox.Size = new Size(308, 23);
             productBrandTextBox.TabIndex = 6;
             // 
             // productModeltextBox
             // 
-            productModeltextBox.Location = new Point(297, 176);
+            productModeltextBox.Location = new Point(260, 132);
+            productModeltextBox.Margin = new Padding(3, 2, 3, 2);
             productModeltextBox.Name = "productModeltextBox";
-            productModeltextBox.Size = new Size(351, 27);
+            productModeltextBox.Size = new Size(308, 23);
             productModeltextBox.TabIndex = 7;
             // 
             // productVersionTextBox
             // 
-            productVersionTextBox.Location = new Point(297, 245);
+            productVersionTextBox.Location = new Point(260, 184);
+            productVersionTextBox.Margin = new Padding(3, 2, 3, 2);
             productVersionTextBox.Name = "productVersionTextBox";
-            productVersionTextBox.Size = new Size(351, 27);
+            productVersionTextBox.Size = new Size(308, 23);
             productVersionTextBox.TabIndex = 8;
             // 
             // productColorTextBox
             // 
-            productColorTextBox.Location = new Point(297, 316);
+            productColorTextBox.Location = new Point(260, 237);
+            productColorTextBox.Margin = new Padding(3, 2, 3, 2);
             productColorTextBox.Name = "productColorTextBox";
-            productColorTextBox.Size = new Size(351, 27);
+            productColorTextBox.Size = new Size(308, 23);
             productColorTextBox.TabIndex = 9;
             // 
             // productPriceTextBox
             // 
-            productPriceTextBox.Location = new Point(297, 388);
+            productPriceTextBox.Location = new Point(260, 291);
+            productPriceTextBox.Margin = new Padding(3, 2, 3, 2);
             productPriceTextBox.Name = "productPriceTextBox";
-            productPriceTextBox.Size = new Size(351, 27);
+            productPriceTextBox.Size = new Size(308, 23);
             productPriceTextBox.TabIndex = 10;
             // 
             // productTypeComboBox
             // 
             productTypeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
             productTypeComboBox.FormattingEnabled = true;
-            productTypeComboBox.Location = new Point(297, 44);
+            productTypeComboBox.Location = new Point(260, 33);
+            productTypeComboBox.Margin = new Padding(3, 2, 3, 2);
             productTypeComboBox.Name = "productTypeComboBox";
-            productTypeComboBox.Size = new Size(351, 28);
+            productTypeComboBox.Size = new Size(308, 23);
             productTypeComboBox.TabIndex = 11;
             // 
             // productImgPictureBox
             // 
             productImgPictureBox.BorderStyle = BorderStyle.FixedSingle;
-            productImgPictureBox.Location = new Point(739, 47);
+            productImgPictureBox.Location = new Point(647, 35);
+            productImgPictureBox.Margin = new Padding(3, 2, 3, 2);
             productImgPictureBox.Name = "productImgPictureBox";
-            productImgPictureBox.Size = new Size(285, 239);
+            productImgPictureBox.Size = new Size(250, 180);
             productImgPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
             productImgPictureBox.TabIndex = 12;
             productImgPictureBox.TabStop = false;
@@ -179,10 +189,10 @@
             loadProductImgButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
             loadProductImgButton.IconSize = 38;
             loadProductImgButton.ImageAlign = ContentAlignment.MiddleLeft;
-            loadProductImgButton.Location = new Point(763, 319);
+            loadProductImgButton.Location = new Point(668, 239);
             loadProductImgButton.Margin = new Padding(1);
             loadProductImgButton.Name = "loadProductImgButton";
-            loadProductImgButton.Size = new Size(248, 53);
+            loadProductImgButton.Size = new Size(217, 40);
             loadProductImgButton.TabIndex = 23;
             loadProductImgButton.Text = " Cargar imagen";
             loadProductImgButton.UseVisualStyleBackColor = false;
@@ -199,14 +209,13 @@
             productsDataGridView.BackgroundColor = Color.Azure;
             productsDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             productsDataGridView.EditMode = DataGridViewEditMode.EditProgrammatically;
-            productsDataGridView.Location = new Point(61, 536);
-            productsDataGridView.Margin = new Padding(3, 4, 3, 4);
+            productsDataGridView.Location = new Point(53, 402);
             productsDataGridView.MultiSelect = false;
             productsDataGridView.Name = "productsDataGridView";
             productsDataGridView.RowHeadersVisible = false;
             productsDataGridView.RowHeadersWidth = 51;
             productsDataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            productsDataGridView.Size = new Size(1093, 331);
+            productsDataGridView.Size = new Size(956, 248);
             productsDataGridView.TabIndex = 24;
             productsDataGridView.SelectionChanged += productsDataGridView_SelectionChanged;
             // 
@@ -214,9 +223,11 @@
             // 
             optionsGroupBox.Controls.Add(removeSelectionButton);
             optionsGroupBox.Controls.Add(editProductButton);
-            optionsGroupBox.Location = new Point(1195, 536);
+            optionsGroupBox.Location = new Point(1046, 402);
+            optionsGroupBox.Margin = new Padding(3, 2, 3, 2);
             optionsGroupBox.Name = "optionsGroupBox";
-            optionsGroupBox.Size = new Size(264, 300);
+            optionsGroupBox.Padding = new Padding(3, 2, 3, 2);
+            optionsGroupBox.Size = new Size(231, 225);
             optionsGroupBox.TabIndex = 25;
             optionsGroupBox.TabStop = false;
             optionsGroupBox.Text = "opciones";
@@ -232,10 +243,10 @@
             removeSelectionButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
             removeSelectionButton.IconSize = 38;
             removeSelectionButton.ImageAlign = ContentAlignment.MiddleLeft;
-            removeSelectionButton.Location = new Point(24, 172);
+            removeSelectionButton.Location = new Point(21, 129);
             removeSelectionButton.Margin = new Padding(1);
             removeSelectionButton.Name = "removeSelectionButton";
-            removeSelectionButton.Size = new Size(222, 53);
+            removeSelectionButton.Size = new Size(194, 40);
             removeSelectionButton.TabIndex = 25;
             removeSelectionButton.Text = "   Quitar selección";
             removeSelectionButton.UseVisualStyleBackColor = false;
@@ -252,10 +263,10 @@
             editProductButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
             editProductButton.IconSize = 38;
             editProductButton.ImageAlign = ContentAlignment.MiddleLeft;
-            editProductButton.Location = new Point(24, 53);
+            editProductButton.Location = new Point(21, 40);
             editProductButton.Margin = new Padding(1);
             editProductButton.Name = "editProductButton";
-            editProductButton.Size = new Size(222, 53);
+            editProductButton.Size = new Size(194, 40);
             editProductButton.TabIndex = 24;
             editProductButton.Text = "Editar";
             editProductButton.UseVisualStyleBackColor = false;
@@ -276,10 +287,10 @@
             addProducButton.IconFont = FontAwesome.Sharp.IconFont.Auto;
             addProducButton.IconSize = 38;
             addProducButton.ImageAlign = ContentAlignment.MiddleLeft;
-            addProducButton.Location = new Point(1053, 144);
+            addProducButton.Location = new Point(921, 108);
             addProducButton.Margin = new Padding(1);
             addProducButton.Name = "addProducButton";
-            addProducButton.Size = new Size(234, 53);
+            addProducButton.Size = new Size(205, 40);
             addProducButton.TabIndex = 26;
             addProducButton.Text = "Crear producto";
             addProducButton.UseVisualStyleBackColor = false;
@@ -287,16 +298,21 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(61, 888);
+            textBox1.Location = new Point(53, 666);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(1108, 27);
+            textBox1.Size = new Size(970, 23);
             textBox1.TabIndex = 28;
+            // 
+            // validationErrorProvider
+            // 
+            validationErrorProvider.ContainerControl = this;
             // 
             // CreateProductForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1503, 927);
+            ClientSize = new Size(1199, 562);
             Controls.Add(textBox1);
             Controls.Add(addProducButton);
             Controls.Add(optionsGroupBox);
@@ -315,11 +331,13 @@
             Controls.Add(producModelLabel);
             Controls.Add(productBrandLabel);
             Controls.Add(productTypeLabel);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "CreateProductForm";
             Text = "CreateProductForm";
             ((System.ComponentModel.ISupportInitialize)productImgPictureBox).EndInit();
             ((System.ComponentModel.ISupportInitialize)productsDataGridView).EndInit();
             optionsGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)validationErrorProvider).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -347,5 +365,6 @@
         private OpenFileDialog openFileImgDialog;
         private FontAwesome.Sharp.IconButton addProducButton;
         private TextBox textBox1;
+        private ErrorProvider validationErrorProvider;
     }
 }
---
+++ ShopManagementSystem/PresentationLayer/Validations/CreateProductValidator.cs
@@ -0,0 +1,36 @@
+﻿using CommonLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Validations
+{
+    public class CreateProductValidator: AbstractValidator<Products>
+    {
+        public CreateProductValidator() 
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(products => products.ProductTypeId)
+          .NotEqual(0).WithMessage("Debe seleccionar un tipo");
+
+            RuleFor(products => products.ProductBrand)
+            .NotEmpty().WithMessage("Debe definir una marca");
+
+            RuleFor(products => products.ProductModel)
+          .NotEmpty().WithMessage("Debe escribir el modelo");
+
+            RuleFor(products => products.ProductVersion)
+          .NotEmpty().WithMessage("Debe especifir la version");
+
+            RuleFor(products => products.ProductColor)
+          .NotEmpty().WithMessage("Debe escribir el color");
+
+            RuleFor(products => products.ProductPrice) 
+           .GreaterThan(0).WithMessage("La cantidad debe ser mayor que cero.");
+        }
+    }
+}
